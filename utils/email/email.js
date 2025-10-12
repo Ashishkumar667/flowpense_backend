ed2b1ab2bc8dfd14dc53d6b6a3749b2756a5b4b9
Ashishkumar667 <ashishkumarinfo6@gmail.com>
2025-10-12 14:36:37 +0530

    update on email

--- utils/email/email.js
+++ utils/email/email.js
@@ -21,56 +21,56 @@
 //   });
 // };
 
-import nodemailer from "nodemailer";
-import { google } from 'googleapis';
-import dotenv from "dotenv";
+// import nodemailer from "nodemailer";
+// import { google } from 'googleapis';
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-const oAuth2Client = new google.auth.OAuth2(
-  process.env.GMAIL_CLIENT_ID,
-  process.env.GMAIL_CLIENT_SECRET,
-  process.env.GMAIL_REDIRECT_URI 
-);
+// const oAuth2Client = new google.auth.OAuth2(
+//   process.env.GMAIL_CLIENT_ID,
+//   process.env.GMAIL_CLIENT_SECRET,
+//   process.env.GMAIL_REDIRECT_URI 
+// );
 
-oAuth2Client.setCredentials({ refresh_token: process.env.GMAIL_REFRESH_TOKEN });
-const accessToken = await oAuth2Client.getAccessToken();
+// oAuth2Client.setCredentials({ refresh_token: process.env.GMAIL_REFRESH_TOKEN });
+// const accessToken = await oAuth2Client.getAccessToken();
 
-export const sendEmail = async (to, subject, html) => {
-  try {
+// export const sendEmail = async (to, subject, html) => {
+//   try {
       
-    const transporter = nodemailer.createTransport({
-        // host: "smtp.gmail.com",
-        // port: 465,
-        //  secure: true,
-         service:"gmail",
-       auth: {
-        type: "OAuth2",
-        user: process.env.EMAIL_USER, 
-        clientId: process.env.GMAIL_CLIENT_ID,
-        clientSecret: process.env.GMAIL_CLIENT_SECRET,
-        refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-        accessToken: accessToken.token,
-      },
-    });
+//     const transporter = nodemailer.createTransport({
+//         // host: "smtp.gmail.com",
+//         // port: 465,
+//         //  secure: true,
+//          service:"gmail",
+//        auth: {
+//         type: "OAuth2",
+//         user: process.env.EMAIL_USER, 
+//         clientId: process.env.GMAIL_CLIENT_ID,
+//         clientSecret: process.env.GMAIL_CLIENT_SECRET,
+//         refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+//         accessToken: accessToken.token,
+//       },
+//     });
 
-    console.log("transporter",transporter);
-    const mailOptions = {
-      from: `"Flowpense" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-    };
-    console.log("mailoptions", mailOptions);
-    const result = await transporter.sendMail(mailOptions);
-    console.log("✅ Email sent:", result.messageId);
+//     console.log("transporter",transporter);
+//     const mailOptions = {
+//       from: `"Flowpense" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject,
+//       html,
+//     };
+//     console.log("mailoptions", mailOptions);
+//     const result = await transporter.sendMail(mailOptions);
+//     console.log("✅ Email sent:", result.messageId);
 
-    return result;
-  } catch (error) {
-    console.error("❌ Email sending error:", error);
-    throw new Error("Failed to send email");
-  }
-};
+//     return result;
+//   } catch (error) {
+//     console.error("❌ Email sending error:", error);
+//     throw new Error("Failed to send email");
+//   }
+// };
 
 // import sgMail from "@sendgrid/mail";
 
@@ -93,33 +93,33 @@ export const sendEmail = async (to, subject, html) => {
 //   }
 // };
 
-// import formData from "form-data";
-// import Mailgun from "mailgun.js";
-// import dotenv from "dotenv";
+import formData from "form-data";
+import Mailgun from "mailgun.js";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
-// const mailgun = new Mailgun(formData);
-// const mg = mailgun.client({
-//   username: "api",
-//   key: process.env.MAILGUN_API_KEY, // from Mailgun dashboard
-// });
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({
+  username: "api",
+  key: process.env.MAILGUN_API_KEY, // from Mailgun dashboard
+});
 
-// export const sendEmail = async (to, subject, html) => {
-//   try {
-//     const domain = process.env.MAILGUN_DOMAIN; // e.g., sandboxXXXX.mailgun.org
+export const sendEmail = async (to, subject, html) => {
+  try {
+    const domain = process.env.MAILGUN_DOMAIN; // e.g., sandboxXXXX.mailgun.org
 
-//     const result = await mg.messages.create(domain, {
-//       from: `"Flowpense" <no-reply@${domain}>`,
-//       to,
-//       subject,
-//       html,
-//     });
+    const result = await mg.messages.create(domain, {
+      from: `"Flowpense" <no-reply@${domain}>`,
+      to,
+      subject,
+      html,
+    });
 
-//     console.log("✅ Email sent via Mailgun:", result.id);
-//     return result;
-//   } catch (error) {
-//     console.error("❌ Mailgun email sending error:", error.message);
-//     throw new Error("Failed to send email");
-//   }
-// };
+    console.log("✅ Email sent via Mailgun:", result.id);
+    return result;
+  } catch (error) {
+    console.error("❌ Mailgun email sending error:", error.message);
+    throw new Error("Failed to send email");
+  }
+};
