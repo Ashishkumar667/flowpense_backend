ddcf43bc2cd3ae949941d2299c3b662711b688eb
Ashishkumar667 <ashishkumarinfo6@gmail.com>
2025-10-13 16:21:18 +0530

    update on nodemailer

--- utils/email/email.js
+++ utils/email/email.js
@@ -22,7 +22,7 @@
 // };
 
 import nodemailer from "nodemailer";
-import { google } from 'googleapis';
+import { google } from "googleapis";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -30,23 +30,23 @@ dotenv.config();
 const oAuth2Client = new google.auth.OAuth2(
   process.env.GMAIL_CLIENT_ID,
   process.env.GMAIL_CLIENT_SECRET,
-  process.env.GMAIL_REDIRECT_URI 
+  process.env.GMAIL_REDIRECT_URI
 );
 
-oAuth2Client.setCredentials({ refresh_token: process.env.GMAIL_REFRESH_TOKEN });
-const accessToken = await oAuth2Client.getAccessToken();
+oAuth2Client.setCredentials({
+  refresh_token: process.env.GMAIL_REFRESH_TOKEN,
+});
 
 export const sendEmail = async (to, subject, html) => {
   try {
-      
+    
+    const accessToken = await oAuth2Client.getAccessToken();
+
     const transporter = nodemailer.createTransport({
-        // host: "smtp.gmail.com",
-        // port: 465,
-        //  secure: true,
-         service:"gmail",
-       auth: {
+      service: "gmail",
+      auth: {
         type: "OAuth2",
-        user: process.env.EMAIL_USER, 
+        user: process.env.EMAIL_USER,
         clientId: process.env.GMAIL_CLIENT_ID,
         clientSecret: process.env.GMAIL_CLIENT_SECRET,
         refreshToken: process.env.GMAIL_REFRESH_TOKEN,
@@ -54,23 +54,22 @@ export const sendEmail = async (to, subject, html) => {
       },
     });
 
-    console.log("transporter",transporter);
     const mailOptions = {
       from: `"Flowpense" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       html,
     };
-    console.log("mailoptions", mailOptions);
+
     const result = await transporter.sendMail(mailOptions);
     console.log("✅ Email sent:", result.messageId);
-
     return result;
   } catch (error) {
     console.error("❌ Email sending error:", error);
     throw new Error("Failed to send email");
   }
 };
+
 
 // import sgMail from "@sendgrid/mail";
 
