04e848391d7c1149d0e56b906af3176723f3ee72
Ashishkumar667 <ashishkumarinfo6@gmail.com>
2025-10-13 16:46:47 +0530

    update on nodemailer.

--- utils/email/email.js
+++ utils/email/email.js
@@ -1,74 +1,76 @@
-// import nodemailer from "nodemailer";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// export const sendEmail = async (to, subject, html) => {
-//   const transporter = nodemailer.createTransport({
-//       host:"smtp.gmail.com",
-//       port:587,
-//       secure: false,
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS,
-//     },
-//   });
-
-//   await transporter.sendMail({
-//     from: `"Flowpense" <${process.env.EMAIL_USER}>`,
-//     to,
-//     subject,
-//     html,
-//   });
-// };
-
 import nodemailer from "nodemailer";
-import { google } from "googleapis";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const oAuth2Client = new google.auth.OAuth2(
-  process.env.GMAIL_CLIENT_ID,
-  process.env.GMAIL_CLIENT_SECRET,
-  process.env.GMAIL_REDIRECT_URI
-);
-
-oAuth2Client.setCredentials({
-  refresh_token: process.env.GMAIL_REFRESH_TOKEN,
-});
-
 export const sendEmail = async (to, subject, html) => {
-  try {
-    
-    const accessToken = await oAuth2Client.getAccessToken();
+  const transporter = nodemailer.createTransport({
+      // host:"smtp.gmail.com",
+      // port:587,
+      // secure: false,
+      service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: process.env.EMAIL_USER,
-        clientId: process.env.GMAIL_CLIENT_ID,
-        clientSecret: process.env.GMAIL_CLIENT_SECRET,
-        refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-        accessToken: accessToken.token,
-      },
-    });
-
-    const mailOptions = {
-      from: `"Flowpense" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-    };
-
-    const result = await transporter.sendMail(mailOptions);
-    console.log("✅ Email sent:", result.messageId);
-    return result;
-  } catch (error) {
-    console.error("❌ Email sending error:", error);
-    throw new Error("Failed to send email");
-  }
+  await transporter.sendMail({
+    from: `"Flowpense" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
 };
+
+// import nodemailer from "nodemailer";
+// import { google } from "googleapis";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const oAuth2Client = new google.auth.OAuth2(
+//   process.env.GMAIL_CLIENT_ID,
+//   process.env.GMAIL_CLIENT_SECRET,
+//   process.env.GMAIL_REDIRECT_URI
+// );
+
+// oAuth2Client.setCredentials({
+//   refresh_token: process.env.GMAIL_REFRESH_TOKEN,
+// });
+
+// export const sendEmail = async (to, subject, html) => {
+//   try {
+    
+//     const accessToken = await oAuth2Client.getAccessToken();
+//     console.log("Access Token:", accessToken.token);
+
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         type: "OAuth2",
+//         user: process.env.EMAIL_USER,
+//         clientId: process.env.GMAIL_CLIENT_ID,
+//         clientSecret: process.env.GMAIL_CLIENT_SECRET,
+//         refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+//         accessToken: accessToken.token,
+//       },
+//     });
+
+//     const mailOptions = {
+//       from: `"Flowpense" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject,
+//       html,
+//     };
+
+//     const result = await transporter.sendMail(mailOptions);
+//     console.log("✅ Email sent:", result.messageId);
+//     return result;
+//   } catch (error) {
+//     console.error("❌ Email sending error:", error);
+//     throw new Error("Failed to send email");
+//   }
+// };
 
 
 // import sgMail from "@sendgrid/mail";
